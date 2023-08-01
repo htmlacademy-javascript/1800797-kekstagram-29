49dde32ae12efdcce2c9f97a35ac2e6a30771ee7
Kirill <89221688844@mail.ru>
2023-08-01 12:40:22 +0500

    2nd check

--- js/util.js
+++ js/util.js
@@ -1,30 +1,6 @@
 // генерация случайного числа
 import { ALERT_SHOW_TIME } from './constance.js';
 
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-// генерация случайного числа с контролем повторений
-const createRandomIdFromRangeGenerator = (min, max) => {
-  const previousValues = [];
-
-  return function () {
-    let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= (max - min + 1)) {
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
-};
-
 const checkLength = (str, lettersLimit) => str.length <= lettersLimit;
 
 const showAlert = (message) => {
@@ -48,6 +24,8 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
 const debounce = (callback, timeoutDelay) => {
   let timeoutId;
   return (...rest) => {
@@ -57,9 +35,8 @@ const debounce = (callback, timeoutDelay) => {
 };
 
 export {
-  getRandomInteger,
-  createRandomIdFromRangeGenerator,
   checkLength,
   showAlert,
+  isEscapeKey,
   debounce
 };
